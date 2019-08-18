130361704cabbd0b5df3287f6943c1357d34bf4c
Thaddeus Metz <51253018+temetz90@users.noreply.github.com>
2019-08-18 00:08:55 -0500

    Add files via upload

---
+++ todo-app/todo-functions.js
@@ -0,0 +1,90 @@
+// Fetch existing todos from localStorage
+const getSavedTodos = function () {
+    const todosJSON = localStorage.getItem('todos')
+
+    if (todosJSON !== null) {
+        return JSON.parse(todosJSON)
+    } else {
+        return []
+    }
+}
+
+// Save todos to localStorage
+const saveTodos = function (todos) {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Render application todos based on filters
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompletedMatch
+    })
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+    document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
+
+    filteredTodos.forEach(function (todo) {
+        document.querySelector('#todos').appendChild(generateTodoDOM(todo))
+    })
+}
+
+// remove a todo
+const removeTodo = function (id) {
+    const todoIndex = todos.findIndex(function (todo) {
+        return todo.id === id
+    })
+
+    if (todoIndex > -1) {
+    todos.splice(todoIndex, 1) }
+}
+
+// Get the DOM elements for an individual note
+const generateTodoDOM = function (todo) {
+    const todoEl = document.createElement('div')
+    const textEl = document.createElement('span')
+    const checkbox = document.createElement('input')
+    const removeButton = document.createElement('button')
+
+    // setup completed todo checkbox
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+
+    checkbox.addEventListener('change', function (e) {
+        todo.completed = checkbox.checked;
+        saveTodos(todos);
+        renderTodos(todos, filters);
+    })
+
+    todoEl.appendChild(checkbox)
+
+    //add the todo text!
+    textEl.textContent = todo.text
+    todoEl.appendChild(textEl)
+
+    //setup a button to the right of the text to remove todo
+    removeButton.textContent = 'Remove';
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', function (e) {
+        removeTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
+    
+    // Check finished todos
+
+    return todoEl
+}
+
+// Get the DOM elements for list summary
+const generateSummaryDOM = function (incompleteTodos) {
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    return summary
+}
